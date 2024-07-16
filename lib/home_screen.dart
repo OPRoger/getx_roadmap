f448e648ac6c96a9e292a2406963b7dd56431c0a
OPRoger <amitku0808@gmail.com>
2024-07-16 15:19:41 +0530

    learn about to add the Get.defaultDialog(), Get.bottomSheet() and change the theme by using the Get

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,7 +17,64 @@ class _Home_ScreenState extends State<Home_Screen> {
       ),
       body: Column(
         children: [
-          
+          Card(
+            child: ListTile(
+              title: Text("getx Dialog alert"),
+              subtitle: Text('getx subtitle alert'),
+              onTap: (){
+                Get.defaultDialog(
+                  title: 'Delete the item',
+                  middleText: "are you sure you want's to delete it ",
+                  titlePadding: EdgeInsets.all(30),
+                  //textConfirm: 'yes',
+                  //textCancel: 'no'
+                    //for the custom
+                  confirm: TextButton(onPressed: (){
+                    Get.back();
+                  }, child: Text('yes')),
+
+                  cancel: TextButton(onPressed: (){
+                    Get.back();
+                  }, child: Text('no')),
+                );
+              },
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text("getx bottomSheet"),
+              subtitle: Text('getx to change the theme'),
+              onTap: (){
+                Get.bottomSheet(
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(30)
+                    ),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.light_mode),
+                          title: Text('ligth theme'),
+                          onTap: (){
+                            Get.changeTheme(ThemeData.light());
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.dark_mode),
+                          title: Text('dark theme'),
+                          onTap: (){
+                            Get.changeTheme(ThemeData.dark());
+                          },
+                        )
+                      ],
+                    ),
+                  )
+
+                );
+              },
+            ),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){
